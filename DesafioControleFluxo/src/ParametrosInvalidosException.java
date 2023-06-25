33a1eadc0080dce1449f5a1b59f4e8e6a3a910c9
Ivan-Pujol <ivan.ivanovich@hotmail.com>
2023-06-25 11:26:59 -0300

    implementação da exceção e da impressão da contagem

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class ParametrosInvalidosException extends Exception{
+  public static String main() {
+   return "Erro de conversão entre tipos diferentes";
+  }
+  
+}
